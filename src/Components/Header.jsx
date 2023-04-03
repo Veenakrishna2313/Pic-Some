c49225612147c989068054038ee7f30a8b730a35
VeenaKrishna2313 <veenakrishna.s@gmail.com>
2023-04-03 00:27:01 -0700

    cart image changed when items added to cart

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,21 @@
 import {Link} from "react-router-dom"
 import React from 'react';
-import ReactDOM from 'react-dom'
+import { useContext} from "react";
+import {Context} from "../Context"
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 
 const Header=()=>{
+
+const {cartItems}=useContext(Context);
+
+const CartImage=cartItems.length>0?<Link to="/cart" className="links"><AddShoppingCartIcon fontSize="large"/></Link>:<Link to="/cart" className="links"><ShoppingCartIcon fontSize="large"/></Link>;
+
   return(
     <header className="header">
       <Link to="/" className="links"><h1>Pic Some</h1></Link>   
-      <Link to="/cart" className="links"><AddShoppingCartIcon fontSize="large"/></Link>
+      {CartImage}
     </header>
   )
 }
